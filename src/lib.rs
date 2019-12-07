e37a05ec71df6c0b7cd5d7172249b61d7d78e6d0
johhonn <jjj.may377@gmail.com>
2019-12-07 17:59:39 -0500

    first upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod utils;
+extern crate rand;
 use std::fmt;
 use wasm_bindgen::prelude::*;
-
+use rand::prelude::*;
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -16,12 +17,26 @@ pub enum Cell {
     Alive = 1,
 }
 
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
+}
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    console_error_panic_hook::set_once();
+} 
+
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
     cells: Vec<Cell>,
 }
+
 #[wasm_bindgen]
 impl Universe{
     
@@ -39,6 +54,47 @@ impl Universe{
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
+       // ...
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+   /** pub fn random_reset(&self){
+        let size=self.height*self.width-1;
+        for i in 0..size {
+                        
+            if rand::random() { // generates a boolean
+                self.cells[i]=Cell::Alive;
+
+            }
+            else{
+                self.cells[i]=Cell::Alive;
+            }
+
+        }
+    }**/
+    pub fn randomReset(&mut self  ){
+         self.cells = (0..self.width * self.height).map(|_i| 
+            match (rand::random()) { // generates a boolean
+               true=>Cell::Alive,
+               false=>Cell::Dead
+            
+            }
+         ).collect();
+    } 
+
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
         for delta_row in [self.height - 1, 0, 1].iter().cloned() {
@@ -54,6 +110,11 @@ impl Universe{
             }
         }
         count
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
     }
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
@@ -88,6 +149,7 @@ impl Universe{
         self.cells = next;
     }
     pub fn new() -> Universe {
+          init_panic_hook();
             let width = 128;
             let height = 128;
 
@@ -114,6 +176,22 @@ impl Universe{
 
 }
 
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
+}
 
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
